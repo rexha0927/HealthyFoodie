0e0b46353080ec400f362bfdb06b3d7fd46fdb73
hou1000fish <hou1000fish@gmail.com>
2026-08-16 23:02:33 +0800

    fix: ux

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 全域設定檔
-const WEB_APP_URL = "GAS_WEB_APP_URL";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyitutr5aBVGSegCTCP3Css3cgr4FGMYbMMGemyx-7mgJ-Fju_izRWob2gS5ElQMCcE/exec";
 
 // 🌟 全域品項設定 (現場盤點與出入庫)
 const products = [
